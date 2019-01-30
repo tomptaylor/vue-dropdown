984650533fdc41fb32c44b7d00201839008928e4
Tom Taylor <tomptaylor@gmail.com>
2019-01-30 16:54:05 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png" /> <HelloWorld />
+    <DropDown dropDownName="State" /> <DropDown dropDownName="District" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import DropDown from "./components/DropDown";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    DropDown
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DropDown.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dd">
+    <label>{{ dropDownName }}</label>
+    <select v-model="selectedRow">
+      <option v-for="row in rows" :value="row.key">{{ row.name }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DropDown",
+  props: ["dropDownName"],
+  data() {
+    return {
+      selectedRow: "",
+      rows: []
+    };
+  },
+  created() {
+    var url = "https://api.jsonbin.io/b/5c514fa04c4430170a939bd9";
+    console.log(this.$props.dropDownName);
+    //     switch (this.$props.dropDownName) {
+    // 	case 'State ':
+    //      url = "https://api.jsonbin.io/b/5c3436e87b31f426f8533a5c";
+    // 		break;
+    //     url = "https://api.jsonbin.io/b/5c514fa04c4430170a939bd9";
+    // 		break;
+    // };
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "secret-key":
+          "$2a$10$036UXxSL9oiSYBbeDqdNd.frX7BvsGTNNDekEX2iUKfHj7U1p1k6G"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        for (let i in json.DATA) {
+          this.rows.push({ name: json.DATA[i][1], key: json.DATA[i][0] });
+        }
+        // console.log(this.rows);
+        // this.states = json;
+      });
+  }
+};
+</script>
